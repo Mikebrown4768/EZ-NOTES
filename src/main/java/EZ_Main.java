387354befc854e6975d6a6c2e5ab83af50c95281
cscx1 <154300319+cscx1@users.noreply.github.com>
2023-12-25 00:29:43 -0500

    ok

--- src/main/java/EZ_Main.java
+++ src/main/java/EZ_Main.java
@@ -1,11 +1,31 @@
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 
 public class EZ_Main {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Set path to acoustic model
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        // Set path to dictionary
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        // Set path to language model
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
 
-		System.out.println("1");
+        try {
+            LiveSpeechRecognizer recognizer = new LiveSpeechRecognizer(configuration);
+            recognizer.startRecognition(true);
 
-	}
+            System.out.println("Speech recognition is ready. Speak now!");
 
+            while (true) {
+                String spokenWords = recognizer.getResult().getHypothesis();
+                if (spokenWords != null && !spokenWords.isEmpty()) {
+                    System.out.println("You said: " + spokenWords);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
